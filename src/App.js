4a549cbb4dd5b2f8718401fad153055da68bda30
unknown <mdtanbir576@gmail.com>
2022-06-16 19:39:21 +0600

    App Route

--- src/App.js
+++ src/App.js
@@ -8,28 +8,77 @@ import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
+import { AuthContext } from "./context/AuthContext";
+import { Navigate } from "react-router-dom";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
+
+  const ProtectedRoute = ({children}) => {
+    const {user} = useContext(AuthContext);
+
+
+    if(!user){
+      return <Navigate to="/login"/>
+    }
+    else{
+      return children;
+    }
+  }
 
   return (
     <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<Home />} />
+            <Route
+              index
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
             <Route path="login" element={<Login />} />
             <Route path="users">
-              <Route index element={<List />} />
-              <Route path=":userId" element={<Single />} />
+              <Route
+                index
+                element={
+                  <ProtectedRoute>
+                    <List />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path=":userId"
+                element={
+                  <ProtectedRoute>
+                    <Single />
+                  </ProtectedRoute>
+                }
+              />
               <Route
                 path="new"
                 element={<New inputs={userInputs} title="Add New User" />}
               />
             </Route>
             <Route path="products">
-              <Route index element={<List />} />
-              <Route path=":productId" element={<Single />} />
+              <Route
+                index
+                element={
+                  <ProtectedRoute>
+                    <List />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path=":productId"
+                element={
+                  <ProtectedRoute>
+                    <Single />
+                  </ProtectedRoute>
+                }
+              />
               <Route
                 path="new"
                 element={<New inputs={productInputs} title="Add New Product" />}
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
-import "./login.scss";
+import "./login.css";
 
 const Login = () => {
   const [credentials, setCredentials] = useState({
@@ -23,8 +23,15 @@ const Login = () => {
     dispatch({ type: "LOGIN_START" });
     try {
       const res = await axios.post("http://localhost:7000/api/auth/login", credentials);
-      dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+      if(res.data.isAdmin){
+          dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
       navigate("/")
+
+      }
+      else{
+          dispatch({ type: "LOGIN_FAILURE", payload: {message : "You are not allowed"}});
+      }
+    
     } catch (err) {
       dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
     }
