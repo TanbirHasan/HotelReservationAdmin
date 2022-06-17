3a430a6824d0dd398b2bb171a30c5e2b8b86770f
unknown <mdtanbir576@gmail.com>
2022-06-17 17:39:30 +0600

    user from dashboard

--- src/pages/newHotel/NewHotel.jsx
+++ src/pages/newHotel/NewHotel.jsx
@@ -1,48 +1,12 @@
 import "./new.scss";
-import axios from "axios"
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import { useState } from "react";
-import { LineAxisOutlined, Preview } from "@mui/icons-material";
 
-const New = ({ inputs, title }) => {
+const NewHotel = ({ inputs, title }) => {
   const [file, setFile] = useState("");
-  const [info, setInfo] = useState({});
 
-  const handleChange = (e) => {
-    setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
-  };
-
-  const handleClick = async (e) => {
-    e.preventDefault();
-    const data = new FormData();
-    data.append("file", file);
-    data.append("upload_preset", "upload");
-    try {
-      const uploadRes = await axios.post(
-        "https://api.cloudinary.com/v1_1/dxpjpngn7/image/upload",
-        data
-      );
-
-      const { url } = uploadRes.data;
-
-      console.log(uploadRes.data);
-
-      const newUser = {
-        ...info,
-        img: url,
-      };
-
-      console.log(newUser);
-
-      await axios.post("http://localhost:7000/api/auth/register", newUser);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  console.log(info);
   return (
     <div className="new">
       <Sidebar />
@@ -79,15 +43,10 @@ const New = ({ inputs, title }) => {
               {inputs.map((input) => (
                 <div className="formInput" key={input.id}>
                   <label>{input.label}</label>
-                  <input
-                    onChange={handleChange}
-                    type={input.type}
-                    placeholder={input.placeholder}
-                    id={input.id}
-                  />
+                  <input type={input.type} placeholder={input.placeholder} />
                 </div>
               ))}
-              <button onClick={handleClick}>Send</button>
+              <button>Send</button>
             </form>
           </div>
         </div>
@@ -96,4 +55,4 @@ const New = ({ inputs, title }) => {
   );
 };
 
-export default New;
+export default NewHotel;
